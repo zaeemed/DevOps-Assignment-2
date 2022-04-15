bf4d1d032c48f6457e3a2024bb1bcb6175762214
Zaeem Ahmed <59163054+zaeemed@users.noreply.github.com>
2022-04-15 23:29:43 +0500

    Update code.py

--- code.py
+++ code.py
@@ -4,4 +4,5 @@ import json
 ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
     response = ec2.describe_availability_zones()
+    print("Hello World")
     return {"statusCode": 200, "body": json.dumps(response)}
